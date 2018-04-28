078960d81fc00e45e89ec1d6e429a2a793df2157
neverchanje <wutao163.com@163.com>
2018-04-28 13:27:15 +0800

    stats: implement metrics API

--- include/dsn/stats/metrics.h
+++ include/dsn/stats/metrics.h
@@ -22,10 +22,12 @@
 #pragma once
 
 #include <cstdint>
-#include <dsn/utility/utils.h>
 #include <cstring>
 #include <atomic>
 
+#include <dsn/utility/utils.h>
+#include <dsn/utility/synchronize.h>
+#include <dsn/utility/autoref_ptr.h>
 
 // Types of metrics
 // ------------------------------------------------------------
@@ -43,6 +45,109 @@
 
 namespace dsn {
 namespace stats {
+
+class metric_prototype
+{
+public:
+    // Simple struct to aggregate the arguments common to all prototypes.
+    // This makes constructor chaining a little less tedious.
+    struct ctor_args
+    {
+        ctor_args(const char *app, const char *name, const char *description)
+            : _app(app), _name(name), _description(description)
+        {
+        }
+
+        const char *const _app;
+        const char *const _name;
+        const char *const _description;
+    };
+
+    const char *app() const { return _args._app; }
+    const char *name() const { return _args._name; }
+    const char *description() const { return _args._description; }
+
+private:
+    const ctor_args _args;
+};
+
+class metric_base : public ref_counter
+{
+public:
+    explicit metric_base(const metric_prototype *proto) {}
+};
+
+// Simple incrementing 64-bit integer.
+// Only use Counters in cases that we expect the count to only increase. For example,
+// a counter is appropriate for "number of transactions processed by the server",
+// but not for "number of transactions currently in flight". Monitoring software
+// knows that counters only increase and thus can compute rates over time, rates
+// across multiple servers, etc, which aren't appropriate in the case of gauges.
+class counter : public metric_base
+{
+public:
+    // Increment the counter by 1.
+    void increment() { add(1); }
+
+    // Add the given value to the counter.
+    void add(uint64_t val) { _val.fetch_add(val, std::memory_order_relaxed); }
+
+    uint64_t get() { return _val.load(std::memory_order_relaxed); }
+
+private:
+    friend class metric_section;
+    explicit counter(const metric_prototype *proto) : metric_base(proto) {}
+
+    std::atomic_uint_fast64_t _val;
+};
+
+class hdr_histogram;
+class histogram : public metric_base
+{
+public:
+    void record(uint64_t val);
+
+private:
+    friend class metric_section;
+    explicit histogram(const metric_prototype *proto) : metric_base(proto) {}
+
+    std::unique_ptr<hdr_histogram> _histogram;
+};
+
+class metric_section
+{
+public:
+    dsn::ref_ptr<counter> find_or_create_counter(metric_prototype *proto)
+    {
+        return find_or_create<counter>(proto);
+    }
+
+    dsn::ref_ptr<histogram> find_or_create_histogram(metric_prototype *proto)
+    {
+        return find_or_create<histogram>(proto);
+    }
+
+private:
+    template <typename T>
+    dsn::ref_ptr<T> find_or_create(metric_prototype *proto)
+    {
+        utils::auto_write_lock l(_lock);
+        auto it = _metric_map.find(proto);
+        if (it == _metric_map.end()) {
+            auto p = dsn::ref_ptr<T>(new T(proto));
+            _metric_map.emplace(proto, p);
+            return p;
+        } else {
+            return static_cast<T *>(it->second.get());
+        }
+    }
+
+private:
+    typedef std::unordered_map<metric_prototype *, dsn::ref_ptr<metric_base>> metric_map;
+
+    mutable utils::rw_lock_nr _lock;
+    metric_map _metric_map;
+};
 
 } // namespace stats
 } // namespace dsn
--- src/stats/test/hdr_histogram_test.cpp
+++ src/stats/test/hdr_histogram_test.cpp
@@ -18,22 +18,3 @@
 // Copyright (c) 2018, Xiaomi, Inc.  All rights reserved.
 // This source code is licensed under the Apache License Version 2.0, which
 // can be found in the LICENSE file in the root directory of this source tree.
-
-#pragma once
-
-#include <dsn/stats/hdr_histogram.h>
-
-namespace dsn {
-namespace stats {
-
-class histogram
-{
-public:
-    histogram(const char *app, const char *section, const char *name, const char *dsptr) {}
-
-private:
-    hdr_histogram _histogram;
-};
-
-} // namespace stats
-} // namespace dsn
